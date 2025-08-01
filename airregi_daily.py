5e8164a70b3f3d61cf603a27ce4f0d07b5a70c01
Kudo-Aki <kudou@sakatura.org>
2025-08-01 19:40:34 +0900

    Update airregi_daily.py

--- airregi_daily.py
+++ airregi_daily.py
@@ -71,7 +71,7 @@ def main():
         page.goto(LOGIN_URL, timeout=60_000)
         page.fill("#account", uid); page.fill("#password", pw)
         page.click("input.primary")
-        page.click("storeList__list__innerBox__name searchTarget")
+        page.click("storeList__list inUse searchList")
         page.wait_for_url(re.compile(r"/(view/top|dashboard)"), timeout=60_000)
 
         # ② 商品別売上
